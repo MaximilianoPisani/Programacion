cdd259805fcaca82d2321a2113733f940cb779e6
MaximilianoPisani <pisanimaximiliano1@gmail.com>
2024-09-26 12:16:49 -0300

    Create clase.cs

---
+++ clase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clase : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Hello World");
+
+        string name = "Maxi";
+        int age = 18;
+        Debug.Log($"Name:{name},Age:{age}");
+        Debug.Log("el nombre es " + name + " la edad es " + age );
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+   
+
+    
+
+    
+    void Update()
+    {
+        
+    }
+}
